ba19884ce75b24c3b0c06a649305c7fd4021cb32
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-11-01 12:39:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
+import Features from "./Components/Features";
+import Workflow from "./Components/Workflow";
+import Pricing from "./Components/Pricing.jsx";
+import Testimonials from "./Components/Testimonials.jsx";
+import Footer from "./Components/Footer.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <Features />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
